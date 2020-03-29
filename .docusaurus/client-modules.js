437cfa82b9d06ad40f9edc1f35614a02c28cddba
Clear2 <12110331@qq.com>
2020-03-29 10:56:03 +0800

    delete

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/zjx/Documents/code-life/src/css/custom.css"),
-  require("/Users/zjx/Documents/code-life/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-];
